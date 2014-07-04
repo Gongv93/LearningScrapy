abfd7c04eba043e7e46273248f7d21bd90b40552
Vincent Gong <gongv93@gmail.com>
2014-07-04 18:23:13 -0400

    SiteTut2

--- example_1/example_1/items.py
+++ example_1/example_1/items.py
@@ -1,6 +1,6 @@
-import scrapy
+from scrapy.item import Item, Field
 
-class DmozItem(scrapy.Item):
-    title = scrapy.Field()
-    link = scrapy.Field()
-    desc = scrapy.Field()
+class DmozItem(Item):
+    title = Field()
+    link = Field()
+    desc = Field()
