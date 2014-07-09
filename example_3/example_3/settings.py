df149f04d53ced940096d119668df9c7cba659cc
Vincent Gong <gongv93@gmail.com>
2014-07-09 13:16:25 -0400

    example_3 Initial commit:

---
+++ example_3/example_3/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for example_3 project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'example_3'
+
+SPIDER_MODULES = ['example_3.spiders']
+NEWSPIDER_MODULE = 'example_3.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'example_3 (+http://www.yourdomain.com)'
